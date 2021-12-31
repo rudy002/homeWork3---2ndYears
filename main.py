75e9ae483fc5a26b956b6990328d817d720ed72b
rudy002 <rudyhaddad.job@gmail.com>
2021-12-31 12:53:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from student import Course, Student
+
+#   nameFile = input("enter the name file you want open")
+nameFile = "School.txt"
+
+
+def checkFileExist(filePath):
+    try:
+        with open(filePath, 'r') as f:
+            return True
+    except FileNotFoundError as e:
+        print("file not found ERROR1")
+        return False
+    except IOError as e:
+        print("file not found ERROR2")
+        return False
+
+
+checkFileExist(nameFile)
+
+file = open(nameFile)
+student = file.read().splitlines()
+
+list_student = []
+all_student = []
+for n in student:
+    name_student = n.split("    ")[0]
+    ID_student = n.split("    ")[1]
+    obj_student = Student(name_student, ID_student)
+    all_course_student = n.split("    ")[2].split(";")
+    for m in all_course_student:
+        name_course = m.split("#")[0]
+        grade_course = int(m.split("#")[1])
+        one_Course = Course(name_course)
+        one_Course.setGrade(grade_course)
+        obj_student.CheckAndUptdate(one_Course)
+
+    list_student.append(obj_student)  # list of object student
+
+print(list_student)
+
+search = []
+while True:
+    search.clear()
+    answer = input("choose answer please\n1- average one Student\n2- average one Course\n3- average all Student\n4- "
+                   "Exit")
+    if int(answer) == 1:
+        search = input("enter the name of the student you want check her Average")
+        
+    elif int(answer) == 2:
+        pass
+    elif int(answer) == 3:
+        pass
+    elif int(answer) == 4:
+        break
+    else:
+        pass
+
+print("End of program. thank you")
---
+++ student.py
@@ -0,0 +1,59 @@
+class Course:
+    def __init__(self, nameCourse, Grade=101):
+        self.nameCourse = nameCourse
+        self.Grade = Grade
+
+    def setGrade(self, grade):
+        if 0 <= int(grade) <= 100:
+            self.Grade = grade
+        else:
+            raise ValueError("error. this grade is incorrect")
+
+    #   from here this is function I added
+    def GetNameCourse(self):
+        return self.nameCourse
+
+    def GetGrade(self):
+        return self.Grade
+
+    def __str__(self):
+        return f'name COURSE :{self.nameCourse}, GRADE : {self.Grade}'
+
+    def __repr__(self):
+        return f'Course({self.nameCourse}:{self.Grade})'
+
+
+class Student:
+    def __init__(self, surname, ID):
+        self.__ID = ID
+        self.Surname = surname
+        self.ListCourse = []
+
+    def GetID(self):
+        return self.__ID
+
+    def addGrade(self, newCourse):
+        if 0 <= newCourse.Grade <= 100:
+            self.ListCourse.append(newCourse)
+        else:
+            raise ValueError("error. this grade is incorrect")
+
+    #   from here, this is no request function form the homework
+
+    def GetSurname(self):
+        return self.Surname
+
+    def CheckAndUptdate(self, course_check):
+        for n in self.ListCourse:
+            if n.GetNameCourse == course_check.nameCourse:
+                n.SetGrade(course_check.Grade)
+                return
+        flag = Course(course_check.nameCourse)
+        flag.setGrade(course_check.Grade)
+        self.ListCourse.append(flag)
+
+    def Average(self):
+        return sum(self.ListCourse[0].Grade) / int(len(self.ListCourse[1].Grade))
+
+    def __repr__(self):
+        return f'STUDENT : {self.Surname} NUMBER ID :{self.__ID} {self.ListCourse}'
